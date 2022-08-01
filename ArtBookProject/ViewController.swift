04e31ccf00b0db26270bc211ce9b57e09ae3193e
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-08-01 13:21:45 +0300

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Nikita Hasan Kafes on 01.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
